4eb4f2cf48f63b92e5bf1d341d8fd33791a12bb4
Jakub Żelezik <jakub.zelezik@gmail.com>
2021-06-02 14:56:12 +0200

    s

--- client/client.py
+++ client/client.py
@@ -7,6 +7,9 @@ import sys
 import numpy as np
 import sounddevice as sd
 
+FRAMESIZE = 256
+SAMPLERATE = 44100
+
 SERVER_IP = '144.126.244.194'
 
 MIC_PORT = 10000
@@ -19,12 +22,16 @@ def server_output_stream(source_q):
     print('Starting server_output_stream')
 
     soc = socket.socket()
+    print('Connecting to MIC_SOCKET...')
     soc.connect((SERVER_IP, MIC_PORT))
+    print('Connected to MIC_SOCKET. Sending ID...')
     soc.send(ID)
+    print('ID set for MIC_SOCKET')
 
     while True:
         try:
             array = source_q.get()
+            #print(f'server_output_stream: Read from queue: {len(array)}')
             soc.send(array[0].tobytes())
         except Exception as e:
             print(e)
@@ -34,13 +41,17 @@ def server_input_stream(target_q):
     print('Starting server_input_stream')
 
     soc = socket.socket()
+    print('connecting to SPEAKER_SOCKET...')
     soc.connect((SERVER_IP, SPEAKER_PORT))
+    print('Connected to SPEAKER_SOCKET. Sending ID...')
     soc.send(ID)
+    print('ID set for SPEAKER_SOCKET')
 
     while True:
         try:
             recording = soc.recv(100000)
             recording = np.frombuffer(recording, dtype=np.float32)
+            #print(f'server_input_stream: Written to queue: {len(recording)}')
             target_q.put((recording.copy(), 'ok'))
         except Exception as e:
             print(e)
@@ -49,42 +60,38 @@ def server_input_stream(target_q):
 def sound_output_stream(source_q):
     print('Starting sound_output_stream')
 
-    def callback(out_data, frame_count, time_info, status):
-        out_data[:] = source_q.get()
-
-    stream = sd.OutputStream(callback=callback, channels=1, dtype=np.float32)
+    stream = sd.OutputStream(channels=1, dtype=np.float32, blocksize=FRAMESIZE, samplerate=SAMPLERATE)
 
     with stream:
         while True:
-            pass
+            data = source_q.get()[0]
+            stream.write(data)
 
 
 def sound_input_stream(target_q):
     print('Starting sound_input_stream')
 
-    def callback(in_data, frame_count, time_info, status):
-        target_q.put((in_data.copy(), status))
-
-    stream = sd.InputStream(callback=callback, channels=1, dtype=np.float32)
+    stream = sd.InputStream(channels=1, dtype=np.float32, blocksize=FRAMESIZE, samplerate=SAMPLERATE)
 
     with stream:
         while True:
-            pass
+            data = stream.read(FRAMESIZE)[0]
+            target_q.put((data, 'ok'))
 
 
 def main():
     try:
         print('Starting...')
-        mic = queue.Queue()
+        mic = queue.Queue(maxsize=1000)
         print('Mic queue created...')
-        speaker = queue.Queue()
+        speaker = queue.Queue(maxsize=1000)
         print('Speaker queue created...')
 
         threads = [
-            threading.Thread(target=sound_input_stream, args=(mic,)),
-            threading.Thread(target=server_input_stream, args=(speaker,)),
-            threading.Thread(target=sound_output_stream, args=(speaker,)),
-            threading.Thread(target=server_output_stream, args=(mic,))
+            threading.Thread(target=sound_input_stream, args=(mic,), name='sound_input_stream'),
+            threading.Thread(target=server_input_stream, args=(speaker,), name='server_input_stream'),
+            threading.Thread(target=sound_output_stream, args=(speaker,), name='sound_output_stream'),
+            threading.Thread(target=server_output_stream, args=(mic,), name='server_output_stream')
         ]
 
         for thread in threads:
@@ -93,8 +100,6 @@ def main():
         while True:
             pass
     except KeyboardInterrupt:
-        for thread in threads:
-            thread.ex
 
         sys.exit('\nInterrupted... Stoping program')
 
