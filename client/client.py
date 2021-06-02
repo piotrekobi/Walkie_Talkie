0851950b7e56c0f96d4c04d24081eaea816aa413
Piotr Jan Paturej <p.paturej@stud.elka.pw.edu.pl>
2021-06-02 12:35:21 +0200

    Disable blocking

--- client/client.py
+++ client/client.py
@@ -7,7 +7,6 @@ import pickle
 import threading
 import time
 
-
 # Create a socket object
 s_mic = socket.socket()
 s_audio = socket.socket()
@@ -16,7 +15,7 @@ s_audio = socket.socket()
 port_mic = 10000
 port_audio = 10001
 
-ID = b'1234'
+ID = b'999'
 
 # connect to the server on local computer
 s_mic.connect(('144.126.244.194', port_mic))
@@ -33,8 +32,7 @@ def receive(soc):
             recording = np.frombuffer(recording, dtype=np.float32)
             print(f'received2: {len(recording)}')
             sr = 44100
-            # sd.play(recording, sr, blocking=True)
-            # time.sleep(0.05)
+            sd.play(recording, sr, blocking=False)
         except Exception as e:
             print(e)
 
@@ -44,15 +42,18 @@ def send(soc):
         try:
             sr = 44100
             duration = 0.05
-            recording = sd.rec(int(duration * sr), samplerate=sr, channels=1, blocking=True)
+            recording = sd.rec(int(duration * sr),
+                               samplerate=sr,
+                               channels=1,
+                               blocking=False)
             print(f'packed: {len(recording.tobytes())}')
             soc.send(recording.tobytes())
         except Exception as e:
             print(e)
 
 
-threading.Thread(target=receive, args=(s_audio,)).start()
-threading.Thread(target=send, args=(s_mic,)).start()
+threading.Thread(target=receive, args=(s_audio, )).start()
+threading.Thread(target=send, args=(s_mic, )).start()
 
 # receive data from the server
 #print(s.recv(17795))
@@ -63,7 +64,6 @@ threading.Thread(target=send, args=(s_mic,)).start()
 #myrecording = pickle.dumps(myrecording)
 #print(len(myrecording))
 #s.send(myrecording)
-
 
 try:
     while True:
