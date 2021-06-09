e1a1a6a05400f68660be10b040ba8b7617a94818
Piotr Jan Paturej <p.paturej@stud.elka.pw.edu.pl>
2021-06-09 18:01:39 +0200

    Add channel list to cli

--- client/cli_app.py
+++ client/cli_app.py
@@ -22,12 +22,15 @@ class GenericView:
     running: bool
     event_bus: EventBus
 
-    def __init__(self, title, bottom_text="  Aby cofnąć do poprzedniego ekranu wciśnij 'q'"):
+    def __init__(
+            self,
+            title,
+            bottom_text="  Aby cofnąć do poprzedniego ekranu wciśnij 'q'"):
         self.title = title
         self.bottom_text = bottom_text
         self.running = False
 
-    def show(self, event_bus, screen):
+    def show(self, screen, event_bus=None):
         self.running = True
         self.screen = screen
         self.event_bus = event_bus
@@ -42,9 +45,12 @@ class GenericView:
 
         y, x = self.screen.getmaxyx()
 
-        self.screen.addstr(1, (x - len(self.title))//2, self.title, curses.A_STANDOUT)
+        self.screen.addstr(1, (x - len(self.title)) // 2, self.title,
+                           curses.A_STANDOUT)
 
-        self.screen.addstr(y - 1, 0, self.bottom_text + ' ' * (x - 1 - len(self.bottom_text)), curses.A_STANDOUT)
+        self.screen.addstr(
+            y - 1, 0, self.bottom_text + ' ' * (x - 1 - len(self.bottom_text)),
+            curses.A_STANDOUT)
         try:
             self.screen.addch(y - 1, x - 1, ' ', curses.A_STANDOUT)
         except curses.error:
@@ -57,8 +63,45 @@ class GenericView:
             self.running = False
 
 
+class ListView(GenericView):
+    def __init__(self,
+                 title,
+                 top_text,
+                 item_list,
+                 bottom_text=" Aby cofnąć do poprzedniego ekranu wciśnij 'q'"):
+
+        super().__init__(title, bottom_text=bottom_text)
+        self.top_text = top_text
+        self.item_list = item_list
+
+    def draw(self):
+        self.screen.clear()
+        self.screen.border(0)
+
+        y, x = self.screen.getmaxyx()
+
+        self.screen.addstr(1, (x - len(self.title)) // 2, self.top_text,
+                           curses.A_STANDOUT)
+
+        for i, item in enumerate(self.item_list):
+            x_pos = 10 * (i // 5) + 1
+            y_pos = 2 + (i % 5)
+            self.screen.addstr(y_pos, x_pos, item, curses.A_NORMAL)
+
+        self.screen.addstr(
+            y - 1, 0, self.bottom_text + ' ' * (x - 1 - len(self.bottom_text)),
+            curses.A_STANDOUT)
+        try:
+            self.screen.addch(y - 1, x - 1, ' ', curses.A_STANDOUT)
+        except curses.error:
+            pass
+
+
 class SelectView(GenericView):
-    def __init__(self, title, options, bottom_text="  Aby opuścić aplikację wciśnij 'q'"):
+    def __init__(self,
+                 title,
+                 options,
+                 bottom_text="  Aby opuścić aplikację wciśnij 'q'"):
         super().__init__(title, bottom_text)
         self.options = options
 
@@ -70,7 +113,8 @@ class SelectView(GenericView):
 
         y, x = self.screen.getmaxyx()
 
-        self.screen.addstr(1, (x - len(self.title))//2, self.title, curses.A_STANDOUT)
+        self.screen.addstr(1, (x - len(self.title)) // 2, self.title,
+                           curses.A_STANDOUT)
 
         pos = 4
         for i in range(len(self.options)):
@@ -82,9 +126,11 @@ class SelectView(GenericView):
             self.screen.addstr(pos, 3, self.options[i].title, style)
             pos += 1
 
-        self.screen.addstr(y-1, 0, self.bottom_text + ' ' * (x - 1 - len(self.bottom_text)), curses.A_STANDOUT)
+        self.screen.addstr(
+            y - 1, 0, self.bottom_text + ' ' * (x - 1 - len(self.bottom_text)),
+            curses.A_STANDOUT)
         try:
-            self.screen.addch(y-1, x-1, ' ', curses.A_STANDOUT)
+            self.screen.addch(y - 1, x - 1, ' ', curses.A_STANDOUT)
         except curses.error:
             pass
 
@@ -123,14 +169,13 @@ class CLIApp:
         curses.start_color()
         self.screen.keypad(1)
 
-        curses.init_pair(1, curses.COLOR_BLACK,
-                         curses.COLOR_WHITE)
+        curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
 
         threading.Thread(target=self.key_events_thread, daemon=True)
         threading.Thread(target=self.time_events_thread, daemon=True)
 
     def run(self):
-        self.tree.show(self.event_bus, self.screen)
+        self.tree.show(self.screen, self.event_bus)
         curses.endwin()
 
     def key_events_thread(self):
@@ -142,5 +187,3 @@ class CLIApp:
         while True:
             time.sleep(0.5)
             self.event_bus.emit(Event('update', None))
-
-
--- client/main.py
+++ client/main.py
@@ -1,29 +1,34 @@
 from call_controler import CallController
-from cli_app import CLIApp, GenericView, SelectView
+from cli_app import CLIApp, GenericView, SelectView, ListView
 from config.server import SERVER_URL, MIC_PORT, SPEAKER_PORT, USER_ID
+import json
 
 if __name__ == '__main__':
-    call_controller = CallController(
-        url=SERVER_URL,
-        mic_port=MIC_PORT,
-        speaker_port=SPEAKER_PORT,
-        user_id=USER_ID
-    )
+    # call_controller = CallController(
+    #     url=SERVER_URL,
+    #     mic_port=MIC_PORT,
+    #     speaker_port=SPEAKER_PORT,
+    #     user_id=USER_ID
+    # )
 
-    call_controller.connect(10)
+    # call_controller.connect(10)
 
-    while True:
-        pass
+    # while True:
+    #     pass
+
+    with open("rest_api/channels.json", "r") as f:
+        channels = json.load(f)
+        available_channels = 15 * [
+            f"{channel['id']}: {channel['name']}" for channel in channels
+        ]
 
     cli_app = CLIApp(
-        SelectView(
-            title='Internetowe Walke-Talke',
-            options=[
-                GenericView(title='Zobacz Dostępne kanały'),
-                GenericView(title='Rozpocznij połączenie'),
-            ]
-        )
-    )
+        SelectView(title='Internetowe Walkie-Talkie',
+                   options=[
+                       ListView(title='Zobacz Dostępne kanały',
+                                top_text="Dostępne kanały:",
+                                item_list=available_channels),
+                       GenericView(title='Rozpocznij połączenie'),
+                   ]))
 
     cli_app.run()
-
