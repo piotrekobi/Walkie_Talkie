3b79773904481d1ce94b62015c0a2f91228054a6
Piotr Jan Paturej <p.paturej@stud.elka.pw.edu.pl>
2021-06-15 19:33:30 +0200

    Add comments to server.py in default_config folder

--- client/default_config/server.py
+++ client/default_config/server.py
@@ -1,6 +1,6 @@
+# Change value of USER_ID to a 5 - 10 digit number
+# Change name of "default_config" to "config"
 SERVER_URL = '144.126.244.194'
 MIC_PORT = 10000
 SPEAKER_PORT = 10001
-
-# Write a number here!
 USER_ID = None
